f44cb10c7787ae89e0294553c2ce73d8b9e21139
Rihan Yang <adam@air20.com>
2018-05-07 10:37:21 -0400

    PSGAN implementation, incomplete

--- src/dcgan/gan.py
+++ src/dcgan/gan.py
@@ -85,37 +85,69 @@ class SGAN(object):
 
             return output.view(-1, 1).squeeze(1)
 
-### four layers PSGAN
+### five layers PSGAN
 class PSGAN(object):
     class _netG(nn.Module):
         def __init__(self, ngpu, nz, ngf, nc):
             super().__init__()
             self.ngpu = ngpu
+            self.nh = 60
+            self.nz_global = 10
+            self.nz_period = 2
+            self.nw = 5
             self.main = nn.Sequential(
-                # input is Z, going into a convolution
-                # dim(Z) = nz, 5, 5
-                nn.ConvTranspose2d(nz, ngf * 4, 5, 2, 2, bias=False),
+                # input is nz x 5 x 5
+                # output is nc x 127 x 127
+                nn.ConvTranspose2d(nz, ngf * 8, 5, 2, 2, bias=False),
+                nn.BatchNorm2d(ngf * 8),
+                nn.ReLU(True),
+                nn.ConvTranspose2d(ngf * 8, ngf * 4, 5, 2, 2, bias=False),
                 nn.BatchNorm2d(ngf * 4),
                 nn.ReLU(True),
-                # state size. (ngf*4) x 9 x 9
+
                 nn.ConvTranspose2d(ngf * 4, ngf * 2, 5, 2, 2, bias=False),
                 nn.BatchNorm2d(ngf * 2),
                 nn.ReLU(True),
-                # state size. (ngf*2) x 17 x 17
+
                 nn.ConvTranspose2d(ngf * 2, ngf, 5, 2, 2, bias=False),
                 nn.BatchNorm2d(ngf),
                 nn.ReLU(True),
-                # state size. (ngf) x 33 x 33
+
                 nn.ConvTranspose2d(ngf, nc, 5, 2, 2, bias=False),
                 nn.Tanh()
-                # state size. (nc) x 65 x 65
+            )
+            self.shared_linear = nn.Linear(nz_global, nh)
+            self.aux1 = nn.Sequential(
+                self.shared_linear,
+                nn.ReLU(True),
+                nn.Linear(nh, nz_period)
             )
 
-        def forward(self, input):
-            if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-                output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-            else:
-                output = self.main(input)
+            self.aux2 = nn.Sequential(
+                self.shared_linear,
+                nn.ReLU(True),
+                nn.Linear(nh, nz_period)
+            )
+
+        def forward(self, l_tensor, g_tensor, phi):
+            # l_tensor : batch_size x nz_local x nw x nw
+            # g_tensor : batch_size x nz_global x 1 x 1
+            # p_tensor : batch_size x nz_period x nw x nw
+            
+            # infer batch_size and nw from input
+            batch_size, _, nw, _ = l_tensor.size()
+
+            g_tensor_expanded = g_tensor.repeat(1, 1, nw, nw)
+            k1 = self.aux1(g_tensor.squeeze())
+            k2 = self.aux2(g_tensor.squeeze())
+            xx = torch.arrange(self.nw).repeat(nw, 1).repeat(batch_size, self.nz_period)
+            yy = torch.arrange(self.nw).repeat(nw, 1).t().repeat(batch_size, self.nz_period)
+            p_tensor = torch.sin(k1*xx + k2*yy + phi)
+
+            input = torch.cat([l_tensor, g_tensor_expanded, p_tensor], 1)
+            # output : batch_size x nz x nw x nw
+            # nz = nz_local + nz_global + nz_period
+            output = self.main(input)
             return output
 
 
@@ -124,20 +156,25 @@ class PSGAN(object):
             super().__init__()
             self.ngpu = ngpu
             self.main = nn.Sequential(
-                # input is (nc) x 65 x 65
+                # input is nc x 127 x 127
+                # output is 1 x 5 x 5
                 nn.Conv2d(nc, ndf, 5, 2, 2, bias=False),
                 nn.BatchNorm2d(ndf),
                 nn.LeakyReLU(0.2, inplace=True),
-                # state size. (ndf) x 33 x 33
+
                 nn.Conv2d(ndf, ndf * 2, 5, 2, 2, bias=False),
                 nn.BatchNorm2d(ndf * 2),
                 nn.LeakyReLU(0.2, inplace=True),
-                # state size. (ndf*2) x 17 x 17
+
                 nn.Conv2d(ndf * 2, ndf * 4, 5, 2, 2, bias=False),
                 nn.BatchNorm2d(ndf * 4),
                 nn.LeakyReLU(0.2, inplace=True),
-                # state size. (ndf*4) x 9 x 9
-                nn.Conv2d(ndf * 4, 1, 5, 2, 2, bias=False),
+
+                nn.Conv2d(ndf * 4, ndf * 8, 5, 2, 2, bias=False),
+                nn.BatchNorm2d(ndf * 8),
+                nn.LeakyReLU(0.2, inplace=True),
+
+                nn.Conv2d(ndf * 8, 1, 5, 2, 2, bias=False),
                 nn.Sigmoid()
             )
 
